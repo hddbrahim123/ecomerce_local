28412a54a417aaaf55404be005a51c7cf9e42303
Ibrahim HADDAD <hddbrahim123@gmail.com>
2021-07-02 15:01:10 +0100

    update order

--- src/Pages/Seller/Order/index.js
+++ src/Pages/Seller/Order/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-
+import {Link} from "react-router-dom"
 import MetaTags from 'react-meta-tags'
 
 import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table"
@@ -8,12 +8,14 @@ import { isEmpty } from "lodash";
 import { getOrders } from "../../../Core/ApiCore/Order";
 import moment from "moment";
 
-import Paginate from '../../../Components/Comon/Paginate'
-
+import CreateOrderStatusModal from './CreateOrderStatusModal'
 
 const Orders = props => {
 
   const [orders, setOrders] = useState([])
+  const [isOpen, setIsOpen] = useState()
+
+  const toggleModal = ()=> setIsOpen(!isOpen)
 
   useEffect(() => {
     getOrders()
@@ -29,7 +31,6 @@ const Orders = props => {
       <MetaTags>
         <title>List Orders |  Sellers</title>
       </MetaTags>
-
       <div className="container-fluid">
         <div className="row">
           <div className="table-rep-plugin ">
@@ -48,29 +49,38 @@ const Orders = props => {
                   <Th scope="col" data-priority="4">paymentStatus</Th>
                   <Th scope="col" data-priority="5">Date</Th>
                   <Th scope="col" data-priority="6">Actions</Th>
+                  <Th scope="col" data-priority="6"></Th>
                 </Tr>
               </Thead>
               <Tbody>
                 {!isEmpty(orders) && orders.map((order,i)=>(
-                <Tr key={i}>
-                  <Td>{order.orderNumber}</Td>
-                  <Td>
-                    <h5 className="my-3">{order.fullName}</h5> 
-                  </Td>
-                  <Td>{order.totalAmount}Dh</Td>
-                  <Td>
-                    <span className="badge bg-primary px-2">{order.status}</span>
-                  </Td>
-                  <Td>{moment(order.Date).calendar()}</Td>
-                  <Td>
-                    <button
-                      className="btn-sm btn-primary btn-rounded"
-                      onClick={()=>{props.history.push(`/seller/order/${order.orderNumber}`)}}
-                    >
-                      View Details
-                    </button>
-                  </Td>
-                </Tr>
+                <>
+                  <CreateOrderStatusModal orderNumber={order.orderNumber} isOpen={isOpen} toggle={toggleModal} />
+                  <Tr key={i}>
+                    <Td>{order.orderNumber}</Td>
+                    <Td>
+                      <h5 className="my-3">{order.fullName}</h5> 
+                    </Td>
+                    <Td>{order.totalAmount}Dh</Td>
+                    <Td>
+                      <span className="badge bg-primary px-2">{order.status}</span>
+                    </Td>
+                    <Td>{moment(order.Date).calendar()}</Td>
+                    <Td>
+                      <button
+                        className="btn-sm btn-primary btn-rounded"
+                        onClick={()=>{props.history.push(`/seller/order/${order.orderNumber}`)}}
+                      >
+                        View Details
+                      </button>
+                    </Td>
+                    <Td>
+                      <Link onClick={toggleModal} to="#" className="text-success fw-bold">
+                        <i className='bx bx-edit'></i>
+                      </Link>
+                    </Td>                  
+                  </Tr> 
+                </>
                 ))}
               </Tbody>        
             </Table>
