28e9906429704003c08a492b2ab90f29dbb3b5de
hp <hp@DESKTOP-540DTGF>
2021-11-19 18:28:25 +0100

    side categories

--- client2/src/Components/ClientLayout/Main2.js
+++ client2/src/Components/ClientLayout/Main2.js
@@ -1,108 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import ListCategories from "./ListCategories";
 
 const baseSiteUrl = window.location.origin.toString() + "/#";
 
 function Main2(props) {
-  const {totalQty, solde, categories, history} = props;
-  let categoryId = 0;
-  //const [categoryId, setCategoryId] = useState(0)
-  let url = window.location.hash.split('/');
-  if (url.length == 3) {
-    //setCategoryId(url[2]);
-    categoryId = url[2];
-  }
-  const goToProducts = (categoryId) => {
-		window.location=(`${baseSiteUrl}/products/${categoryId}/`);
-		window.location.reload();
-	}
+  const {totalQty, solde, history} = props;
+  // let categoryId = 0;
+  // let url = window.location.hash.split('/');
+  // if (url.length == 3) {
+  //   categoryId = url[2];
+  // }
+  // const goToProducts = (categoryId) => {
+	// 	window.location=(`${baseSiteUrl}/products/${categoryId}/`);
+	// 	window.location.reload();
+	// }
   return (
     <div id="mainBody">
       <div className="container">
         <div className="row">
-          {/* Sidebar ================================================== */}
-          <div id="sidebar" className="span3">
-            <div className="well well-small">
-              <a id="myCart" href={`${baseSiteUrl}/cart`}>
-                <img src="images/ico-cart.png" alt="cart" />{totalQty} Articles en panier 
-                <span className="badge badge-warning pull-right">{solde} Dh</span>
-              </a>
-            </div>
-            {/* <ul id="sideManu" className="nav nav-tabs nav-stacked">
-              <li className="subMenu"><a> ELECTRONICS [230]</a>
-                <ul>
-                <li><a href=""><i className="icon-chevron-right"></i>Cameras (100) </a></li>
-                </ul>
-              </li>
-              <li className="subMenu"><a> CLOTHES [840] </a>
-              <ul>
-                <li><a href=""><i className="icon-chevron-right"></i>Women's Clothing (45)</a></li>
-                <li><a href=""><i className="icon-chevron-right"></i>Women's Shoes (8)</a></li>												
-              </ul>
-              </li>
-              <li className="subMenu"><a href="">HEALTH &amp; BEAUTY [18]</a></li>
-            </ul> */}
-            {/* List Categories  */}
-            <ListCategories categoryId={categoryId} categories={categories} />
-            {/* <br />
-            <div className="thumbnail">
-              <img
-                src="images/products/panasonic.jpg"
-                alt="Bootshop panasonoc New camera"
-              />
-              <div className="caption">
-                <h5>Panasonic</h5>
-                <h4 style={{textAlign:"center"}}>
-                  <a className="btn" href="product_details.html">
-                    {" "}
-                    <i className="icon-zoom-in"></i>
-                  </a>{" "}
-                  <a className="btn" href="#">
-                    Add to <i className="icon-shopping-cart"></i>
-                  </a>{" "}
-                  <a className="btn btn-primary" href="#">
-                    $222.00
-                  </a>
-                </h4>
-              </div>
-            </div>
-            <br />
-            <div className="thumbnail">
-              <img
-                src="images/products/kindle.png"
-                title="Bootshop New Kindel"
-                alt="Bootshop Kindel"
-              />
-              <div className="caption">
-                <h5>Kindle</h5>
-                <h4 style={{textAlign:"center"}}>
-                  <a className="btn" href="product_details.html">
-                    {" "}
-                    <i className="icon-zoom-in"></i>
-                  </a>{" "}
-                  <a className="btn" href="#">
-                    Add to <i className="icon-shopping-cart"></i>
-                  </a>{" "}
-                  <a className="btn btn-primary" href="#">
-                    $222.00
-                  </a>
-                </h4>
-              </div>
-            </div>
-            <br />
-            <div className="thumbnail">
-              <img
-                src="images/payment_methods.png"
-                title="Bootshop Payment Methods"
-                alt="Payments Methods"
-              />
-              <div className="caption">
-                <h5>Payment Methods</h5>
-              </div>
-            </div> */}
-          </div>
-          {/* Sidebar end=============================================== */}
+          
           {props.children}
         </div>
       </div>
--- client2/src/Components/ClientLayout/index.js
+++ client2/src/Components/ClientLayout/index.js
@@ -1,82 +1,97 @@
 import React, { useEffect, useState } from "react";
-import { withRouter } from "react-router";
-
-// import Footer from "./Footer";
-// import Header from "./header";
-
-import Header2 from "./Header2";
-import Main2 from "./Main2";
-import Footer2 from "./Footer2";
-import { getCategories } from "../../Core/ApiCore/Category";
+import Header2 from "../../../Components/ClientLayout/Header2";
+import FeaturedProducts from "./FeaturedProducts";
+import Main2 from "../../../Components/ClientLayout/Main2";
+import Footer2 from "../../../Components/ClientLayout/Footer2";
+import MyCarousel from "./MyCarousel";
 import { useSelector } from "react-redux";
+import { getCategories } from "../../../Core/ApiCore/Category";
+import { getLatestProductsViewUser } from "../../../Core/ApiCore/ProductHome";
+import LatestProducts from "./LatestProducts";
+import ListCategories from "./ListCategories";
+import { Link } from "react-router-dom";
 
-const ClientLayout = (props) => {
-
-  const [language] = useState(
-    localStorage.getItem("language") ?? "Fr"
-  );
-
-  const totalQty = useSelector(state => state.Cart.totalQty);
-	const solde = useSelector(state => state.Cart.solde);
-  const [categories, setCategories] = useState([]);
+const Index = () => {
+  const [language] = useState(localStorage.getItem("language") ?? "Fr");
 
   // document.head.innerHTML = `<meta charset="utf-8">
-	// <title>ETNT Shopping</title>
-	// <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	// <meta name="title" content="ETNT Shopping" />
-	// <meta name="description" content="ETNT Shopping" />
-	// <link rel="apple-touch-icon" href="logo192.png" />
-	// <meta name="author" content="">
-	// <link rel="manifest" href="manifest.json" />
-	// <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" />
-	// <link href="css/base.css" rel="stylesheet" media="screen" />
-	// <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
-	// <link href="css/font-awesome.css" rel="stylesheet" type="text/css">
-	// <link href="js/prettify.css" rel="stylesheet">
-	// <link rel="shortcut icon" href="images/ico/favicon.ico">
-	// <style type="text/css" id="enject"></style>`;
+  // <title>ETNT Shopping</title>
+  // <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  // <meta name="title" content="ETNT Shopping" />
+  // <meta name="description" content="ETNT Shopping" />
+  // <link rel="apple-touch-icon" href="logo192.png" />
+  // <meta name="author" content="">
+  // <link rel="manifest" href="manifest.json" />
+  // <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" />
+  // <link href="css/base.css" rel="stylesheet" media="screen" />
+  // <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+  // <link href="css/font-awesome.css" rel="stylesheet" type="text/css">
+  // <link href="js/prettify.css" rel="stylesheet">
+  // <link rel="shortcut icon" href="images/ico/favicon.ico">
+  // <style type="text/css" id="enject"></style>`;
+
+  const totalQty = useSelector(state => !!state.Cart.totalQty ? state.Cart.totalQty : 0);
+  const solde = useSelector(state => state.Cart.solde);
+  const [categories, setCategories] = useState([]);
+  const [latestProducts, setLatestProducts] = useState([]);
+  const [featuredProducts, setFeaturedProducts] = useState([]);
 
   useEffect(() => {
-    getCategories(true,true).then(res =>{
+    getCategories(true, true).then(res => {
       if (res && res.length) {
         setCategories(res);
       } else {
         setCategories([]);
       }
     });
+    getLatestProductsViewUser(12).then((res) => {
+      if (res && res.length) {
+        setLatestProducts(res);
+        setFeaturedProducts(res);
+      }
+    });
+
+    // getProductsTopSale(12).then((res) => setProductsTopSale(res));
+    // getProductsTopRating(12).then((res) => setProductsTopRating(res));
   }, [])
 
-	const [filter, setFilter] = useState({
-		search:'',
-		categoryId:undefined
-	})
+  const [filter, setFilter] = useState({
+    search: '',
+    categoryId: undefined
+  })
 
   return (
     <div>
       <Header2 language={language} totalQty={totalQty} solde={solde} filter={filter} setFilter={setFilter} />
-      <Main2 language={language} totalQty={totalQty} solde={solde} categories={categories}>{props.children}</Main2>
-      <Footer2 language={language} />
-
-      {/* <div className="container">
-        <div className="row">
-          <div className="col-24 col-md-12">
-            <Header
-              language={language}
-              handleStoreLanguage={storeLanguageInLocalStorage}
-            />
-            <main id="page__topbar">{props.children}</main>
-            <Footer language={language} />
+      {/* Sidebar ================================================== */}
+      <div id="sidebar" className="span3">
+        <div className="main">
+          <div className="catt">
+            <div className="well well-small">
+              <Link id="myCart" to="/cart">
+                <img src="images/ico-cart.png" alt="cart" />{totalQty} Articles en panier
+                <span className="badge badge-warning pull-right">{solde} Dh</span>
+              </Link>
+            </div>
+            <ListCategories categories={categories} />
+          </div>
+          <div>
+            <MyCarousel language={language} />
           </div>
         </div>
-      </div> */}
+      </div>
+      {/* Sidebar end=============================================== */}
+
+      <Main2 language={language} totalQty={totalQty} solde={solde}  >
+        <div className="span9">
+          {/* <DemoCarousel /> */}
+          <FeaturedProducts products={featuredProducts} />
+          <LatestProducts products={latestProducts} />
+        </div>
+      </Main2>
+      <Footer2 language={language} />
     </div>
   );
-
-  // function storeLanguageInLocalStorage(language) {
-  //   setLanguage(language);
-  //   localStorage.setItem("language", language);
-  //   window.location.reload();
-  // }
 };
 
-export default withRouter(ClientLayout);
+export default Index;
